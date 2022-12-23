1ce795217a3ab30b25cfbe7cf8c929330b497854
Hitesh <hitesh@pop-os.localdomain>
2022-12-23 23:57:13 +0530

    new add

--- script.js
+++ script.js
@@ -1,68 +1,75 @@
 playlist = {
-    'song_1' : 'song/001.mp3',
-    'song_2' : 'song/002.mp3',
-    'song_3' : 'song/003.mp3',
-    'song_4' : 'song/004.mp3',
+    'song_1': 'song/001.mp3',
+    'song_2': 'song/002.mp3',
+    'song_3': 'song/003.mp3',
+    'song_4': 'song/004.mp3',
+}
+
+window.onload = function () {
+    play_audio('play');
+}
+
+window.onpause = function () {
+    play_audio('stop');
+}
+
+window.onclose = function () {
+    play_audio('stop');
+}
+
+
+
+var min = 0;
+var max = 3;
+var random =
+    Math.floor(Math.random() * (+max + 1 - +min)) + +min;
+
+
+function play_audio(task) {
+    if (task == 'play') {
+        $(".my_audio").trigger('play');
     }
-
-    window.onload=function(){
-        play_audio('play');
+    if (task == 'stop') {
+        $(".my_audio").trigger('pause');
+        $(".my_audio").prop("currentTime", 0);
     }
-    
-    window.onpause = function () {
-        play_audio('stop');
-    }
+}
 
-    window.onclose = function () {
-        play_audio('stop');
-    }
+keys = Object.keys(playlist);
+$('.my_audio').append("<source id='sound_src' src=" + playlist[keys[random]] + " type='audio/mpeg'>");
 
 
-    // function play_audio(task) {
-    //     if (task == 'play') {
-    //         $(".my_audio").trigger('play');
-    //     }
-    //     if (task == 'stop') {
-    //         $(".my_audio").trigger('pause');
-    //         $(".my_audio").prop("currentTime", 0);
-    //     }
-    // }
-
-    keys = Object.keys(playlist);
-    $('.my_audio').append("<source id='sound_src' src=" + playlist[keys[0]] + " type='audio/mpeg'>");
+count = 0;
+$('.my_audio').on('ended', function () {
+    count++;
+    $("#sound_src").attr("src", playlist[keys[count]])[0];
+    $(".my_audio").trigger('load');
+    play_audio('play');
+});
 
 
-    count = 0;
-    $('.my_audio').on('ended', function () {
-        count++;
-        $("#sound_src").attr("src", playlist[keys[count]])[0];
-        $(".my_audio").trigger('load');
-        play_audio('play');
-    });
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slick').slick({
-      dots: false,
-      infinite: true,
-      autoplay: true,
-      speed: 100,
-      fade: true,
-      prevArrow: false, 
-      nextArrow: false, 
-      cssEase: 'linear'
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 100,
+        fade: true,
+        prevArrow: false,
+        nextArrow: false,
+        cssEase: 'linear'
     });
-  });
+});
 
-  $(document).ready(function(){
+$(document).ready(function () {
     $('.slick2').slick({
-      dots: false,
-      infinite: true,
-      autoplay: true,
-      speed: 100,
-      fade: true,
-      prevArrow: false, 
-      nextArrow: false, 
-      cssEase: 'linear'
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 100,
+        fade: true,
+        prevArrow: false,
+        nextArrow: false,
+        cssEase: 'linear'
     });
-  });
+});
